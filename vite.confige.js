02dcffd7620a5822da9da94d6de6a646ba8cf21b
Mykyta <nik.from.varga@gmail.com>
2025-07-10 12:09:49 +0300

    test

--- vite.confige.js
+++ vite.confige.js
@@ -1,65 +1,48 @@
 import { defineConfig } from 'vite';
 import { glob } from 'glob';
-import path from 'path';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
+import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
   return {
-    root: path.resolve(__dirname, 'src'),
-    base: command === 'build' ? '/goit-js-hw-10/' : '/',
-    
+    define: {
+      [command === 'serve' ? 'global' : '_global']: {},
+    },
+    root: 'src',
     build: {
+      sourcemap: true,
+      rollupOptions: {
+        input: glob.sync('./src/*.html'),
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return 'vendor';
+            }
+          },
+          entryFileNames: chunkInfo => {
+            if (chunkInfo.name === 'commonHelpers') {
+              return 'commonHelpers.js';
+            }
+            return '[name].js';
+          },
+          assetFileNames: assetInfo => {
+            if (assetInfo.name && assetInfo.name.endsWith('.html')) {
+              return '[name].[ext]';
+            }
+            return 'assets/[name]-[hash][extname]';
+          },
+        },
+      },
       outDir: '../dist',
       emptyOutDir: true,
-      
-      // Критически важный блок для копирования всех HTML
-      rollupOptions: {
-        input: Object.fromEntries(
-          // Находим все HTML-файлы в src и подпапках
-          glob.sync(path.join(__dirname, 'src', '**/*.html')).map(file => [
-            // Имя файла без пути (например: 'about' для about.html)
-            path.relative('src', file).replace(/\.html$/, ''),
-            // Абсолютный путь к файлу
-            file
-          ])
-        ),
-        output: {
-          // Сохраняем оригинальные имена HTML-файлов
-          entryFileNames: 'assets/js/[name]-[hash].js',
-          chunkFileNames: 'assets/js/[name]-[hash].js',
-          assetFileNames: (assetInfo) => {
-            const extType = assetInfo.name.split('.').pop();
-            
-            // Для HTML сохраняем оригинальную структуру
-            if (extType === 'html') {
-              const relativePath = path.relative(
-                path.join(__dirname, 'src'), 
-                assetInfo.source
-              );
-              return relativePath;
-            }
-            
-            // Для CSS
-            if (['css', 'scss'].includes(extType)) {
-              return 'assets/css/[name]-[hash][extname]';
-            }
-            
-            // Для остальных ресурсов
-            return 'assets/[name]-[hash][extname]';
-          }
-        }
-      }
     },
-    
     plugins: [
       injectHTML(),
-      FullReload(['src/**/*.html'])
+      FullReload(['./src/**/**.html']),
+      SortCss({
+        sort: 'mobile-first',
+      }),
     ],
-    
-    server: {
-      port: 5173,
-      open: '/index.html'
-    }
   };
 });
